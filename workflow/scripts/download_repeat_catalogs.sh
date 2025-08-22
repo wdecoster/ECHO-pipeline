b66d46652ce2b85c88c50bb3d7f603d66cc54f1e
Leena Putzeys <leena.putzeys@mumc.nl>
2025-08-22 15:30:44 +0200

    Fixed typo in download_repeat_catalogs.sh

--- workflow/scripts/download_repeat_catalogs.sh
+++ workflow/scripts/download_repeat_catalogs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Download repeat catalog zip file from Zenodo to current directory
-wget -O repeat_catalogs.zip "https://zenodo.org/record/16925640/files/repeat_catalogs.zip?download=1"
+wget -O repeat-catalogs.zip "https://zenodo.org/record/16925640/files/repeat-catalogs.zip?download=1"
 
 # Unzip - this will create the 'repeat_catalogs/' folder in the current directory
-unzip -o repeat_catalogs.zip
+unzip -o repeat-catalogs.zip
 
 echo "Repeat catalogs downloaded and extracted successfully"
