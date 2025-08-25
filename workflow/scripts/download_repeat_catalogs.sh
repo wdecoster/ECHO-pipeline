6ab29cf66c264cd5df15b19720b9ec393ade43fc
Leena <leena.putzeys@mumc.nl>
2025-08-25 12:44:50 +0200

    Modify tldr.yaml and update DAG

--- workflow/scripts/download_repeat_catalogs.sh
+++ workflow/scripts/download_repeat_catalogs.sh
@@ -6,4 +6,7 @@ wget -O repeat-catalogs.zip "https://zenodo.org/record/16925640/files/repeat-cat
 # Unzip - this will create the 'repeat_catalogs/' folder in the current directory
 unzip -o repeat-catalogs.zip
 
+# Remove zip file after extraction
+rm repeat-catalogs.zip
+
 echo "Repeat catalogs downloaded and extracted successfully"
